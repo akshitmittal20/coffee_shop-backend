6368ae377fdbc6c52716114e601b8928897b7071
akshitmittal20 <akshitmittal20@gmail.com>
2024-06-26 15:13:18 +0530

    cicd change

--- src/server.js
+++ src/server.js
@@ -27,5 +27,5 @@ app.use('/api', coffeeShopRoutes);
 // Error Handler Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
